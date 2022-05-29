74f8a4e8475501b03ef2d827f24ab59f4eade0be
Ayush Prakash <payush381@gmail.com>
2022-05-29 17:03:08 +0530

    Added firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC1OvHM1V8e2HekN2nJ2qMfLWLqPJNgNOo",
+  authDomain: "the-westworld.firebaseapp.com",
+  projectId: "the-westworld",
+  storageBucket: "the-westworld.appspot.com",
+  messagingSenderId: "542481451168",
+  appId: "1:542481451168:web:f87051ff513d1dcaf2a1c5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
