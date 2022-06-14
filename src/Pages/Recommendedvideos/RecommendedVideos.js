1978bfec0623a9c48148f95dcd2ce9b40225a243
Ayush Prakash <payush381@gmail.com>
2022-06-14 15:47:54 +0530

    updated js

--- src/Pages/Recommendedvideos/RecommendedVideos.js
+++ src/Pages/Recommendedvideos/RecommendedVideos.js
@@ -8,27 +8,24 @@ import db from "../../firebase";
 
 function RecommendedVideos() {
 
-    const [likedposts , setLikedPosts] = useState([]);
+    const [videos , setvideos] = useState([]);
 
     useEffect(() => {
         onSnapshot(collection(db, "videos"), (snapshot) => {
-             console.log(snapshot.docs)
-             setLikedPosts(snapshot.docs.map((doc) => ({
+             setvideos(snapshot.docs.map((doc) => ({
 
                        id: doc.id,
                        data: doc.data(),
                      })))
             })
             },[]);
-
-            console.log(likedposts);
     
 
   return (
     <div className="recommendedvideo-container">
         <h2>Recommended</h2>
         <div className="recommended-video">
-        {likedposts.map(({ id , data : { name , imgsrc , channel , timestamp , views , channelimg}})  =>  (
+        {videos.map(({ id , data : { name , imgsrc , channel , timestamp , views , channelimg}})  =>  (
 
                             <VideoCard
                             key={id} 
