402206af461a997a761af8a8802053ed245f31c0
Ayush Prakash <payush381@gmail.com>
2022-07-17 16:07:05 +0530

    Added Authentication

--- src/Pages/AllRoutes/AllRoutes.js
+++ src/Pages/AllRoutes/AllRoutes.js
@@ -6,23 +6,27 @@ import Explore from "../Explore/Explore";
 import History from "../History/History";
 import Homepage from "../Homepage/Homepage";
 import Like from "../Like/Like";
+import Login from "../Login/Login";
 import Playlist from "../Playlist/Playlist";
 import PlaylistVideos from "../Playlist/PlaylistVideos";
 import VideoRendering from "../ShowVideo/VideoRendering";
+import Signup from "../Signup/Signup";
 import WatchLater from "../WatchLater/WatchLater";
 
 function AllRoutes() {
   return (
     <div>
       <Routes>
+        <Route path='/signup' element={<Signup />} />
         <Route path='/history' element={<History />} />
+        <Route path='/' element={<Login />} />
         <Route path='/watchlater' element={<WatchLater />} />
         <Route path='/like' element={<Like />} />
         <Route path='/playlistvideos' element={<PlaylistVideos />} />
         <Route path='/playlist' element={<Playlist />} />
         <Route path='/showvideo' element={<VideoRendering />} />
         <Route path='/explore' element={<Explore />} />
-        <Route path='/' element={<Homepage />} />
+        <Route path='/homepage' element={<Homepage />} />
       </Routes>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
+/** @format */
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { VideoProvider } from "./Pages/Context/VideoContext";
+import { AuthContextProvider } from "./Pages/Context/Authcontext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <VideoProvider>
-        <App />
-      </VideoProvider>
+      <AuthContextProvider>
+        <VideoProvider>
+          <App />
+        </VideoProvider>
+      </AuthContextProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
